ad8c9095b1e4c50e7177f16d5d4be026edd87112
MashaSamoylova <mashanedyak@gmail.com>
2019-05-16 01:42:54 +0700

    Add task 2

---
+++ priveleges.sql
@@ -0,0 +1,49 @@
+/*
+1) Пользователя для управления только данными во всех БД на сервере.
+имя: admin
+хост: любой хост
+без права передавать привилегии
+ */
+
+CREATE USER 'admin'@'%';
+GRANT ALL PRIVILEGES ON  *.* TO 'admin'@'%';
+
+/*
+2) Пользователя для управления схемой firewallLogs
+имя: firewallAdmin
+хост: localhost
+с правом передавать привелегии
+ */
+
+CREATE USER 'firewallAdmin'@'localhost';
+GRANT ALL PRIVILEGES ON firewallLogs.* TO 'firewallAdmin'@'localhost'
+GWITH GRANT OPTION;
+
+/*
+3) Пользователя для загрузки данных в таблицы схемы passwordsHashes
+имя: pwdLoader
+хост: любой хост
+без права передавать привилегии
+ */
+
+CREATE USER 'pwdLoader'@'%';
+GRANT UPDATE, INSERT  ON passwordsHashes.* TO 'pwdLoader'@'%';
+
+/*
+4) Пользователя для анализа данных в схеме passwordsHashes
+имя: pwdAnalyst
+с правом передавать привилегии
+ */
+
+
+CREATE USER 'pwdAnalyst'@'%';
+GRANT SELECT ON passwordsHashes.* TO 'pwdAnalyst'@'%' WITH GRANT OPTION;
+
+/*
+5) Анонимного пользователя
+без права передавать привелегии
+ */
+
+CREATE USER ''@'%';
+GRANT ALL ON *.* TO ''@'%';
+
