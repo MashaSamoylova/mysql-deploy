88b4b14ad6f939fc53a6b4aa6ebfc1b66f9a39de
m.nedyak <m.nediack@bi.zone>
2019-05-23 12:21:47 +0700

    Remove unescessary commands

--- 3.sql
+++ 3.sql
@@ -8,31 +8,25 @@
 
 /* 1) admin@'%' - требуется подтверждение владельца и издателя сертификата 
  */
-GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' IDENTIFIED BY '1234';
+
 ALTER USER 'admin'@'%' REQUIRE SSL;
 ALTER USER 'admin'@'%' REQUIRE ISSUER '/CN=MySQL_Server_5.7.25_Auto_Generated_CA_Certificate';
 ALTER USER 'admin'@'%' REQUIRE SUBJECT '/CN=MySQL_Server_5.7.25_Auto_Generated_Client_Certificate';
 
 /*2) firewallAdmin@'%' - требуется только защищённое соединение
 */
-GRANT ALL PRIVILEGES ON *.* TO 'firewallAdmin'@'%' IDENTIFIED BY '1234';
 ALTER USER 'firewallAdmin'@'%' REQUIRE SSL;
 
 /*
 3) pwdAnalyst@'%' - требуется подтверждение: использование любого валидного электронного сертификата в рамках инфраструктуры открытых ключей
  */
 
-GRANT ALL PRIVILEGES ON *.* TO 'pwdAnalyst'@'%';
 ALTER USER 'pwdAnalyst'@'%' REQUIRE X509;
 
 /*
 4) pwdLoader@'%' - требуется подтверждение издателя сертификата, причём издатель должен быть другим нежели чем издатель для сертификата сервера MySQL
  */
-CREATE USER 'pwdLoader'@'%' IDENTIFIED BY "1234";
 ALTER USER 'pwdLoader'@'%' REQUIRE ISSUER '/CN=olololo';
-
-
-
 
 
 /*
